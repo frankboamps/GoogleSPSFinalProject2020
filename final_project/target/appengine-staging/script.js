2192aea30b505ceb00caeff242b304015776c56c
Majdi Nagi <majdigamil1998@gmail.com>
2020-07-14 19:00:03 -0400

    slideshow is working now.

--- final_project/target/appengine-staging/script.js
+++ final_project/target/appengine-staging/script.js
@@ -130,36 +130,26 @@ function buildInfoWindowInput(lat, lng) {
   return containerDiv;
 }
 
+var slideIndex = 0;
+// showSlides();
 
-/*slide show functions*/
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-  getCauses();
-  printComment();
-}
-
-function showSlides(n) {
+function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
 
 function fetchBlobstoreUrlAndShowForm() {
     fetch('/blobstore-upload-url').then(response => response.text()).then((quote) => {
@@ -177,7 +167,11 @@ function getCauses(){
         causeListElement.appendChild(createPostUnit(obj));
     }
   });
+    // getCauses();
+    showSlides();
+  printComment();
 }
+
 
 function createPostUnit(obj){
     const divElement = document.createElement('div');
