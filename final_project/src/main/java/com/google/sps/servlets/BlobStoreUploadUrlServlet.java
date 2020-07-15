4c74d3c0a9aa0b0c18a94a007a7b3deb55d9ffe2
Majdi Nagi <majdigamil1998@gmail.com>
2020-07-15 09:57:28 -0400

    everything is updated in this version

--- final_project/src/main/java/com/google/sps/servlets/BlobStoreUploadUrlServlet.java
+++ final_project/src/main/java/com/google/sps/servlets/BlobStoreUploadUrlServlet.java
@@ -1,5 +1,4 @@
 package com.google.sps.servlets;
-
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import java.io.IOException;
@@ -8,8 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-
 /**
  * When the fetch() function requests the /blobstore-upload-url URL, the content of the response is
  * the URL that allows a user to upload a file to Blobstore. If this sounds confusing, try running a
